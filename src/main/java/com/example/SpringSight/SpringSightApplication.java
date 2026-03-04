de239202d5c8e1e18217c14a6f6f7d2238211680
animus212 <mohanadhussein536@gmail.com>
2026-03-04 23:00:17 +0200

    first commit

---
+++ src/main/java/com/example/SpringSight/SpringSightApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringSight/SpringSightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
