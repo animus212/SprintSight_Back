cf434b02a54db109e522bf803ce0684ffebe4c87
animus <mohanadhussein536@gmail.com>
2026-03-05 00:03:43 +0200

    added env variables

--- src/main/java/com/example/SpringSight/SpringSightApplication.java
+++ src/main/java/com/example/SpringSight/SpringSightApplication.java
@@ -1,5 +1,6 @@
 package com.example.SpringSight;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSightApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSightApplication.class, args);
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+
+        SpringApplication.run(SpringSightApplication.class, args);
 	}
 
 }
