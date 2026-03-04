69520d1dd3de8d6be0795b35bfcbdb22237c4d89
animus <mohanadhussein536@gmail.com>
2026-03-05 00:10:21 +0200

    whoopsie

--- src/main/java/com/example/SpringSight/SprintSightApplication.java
+++ src/main/java/com/example/SpringSight/SprintSightApplication.java
@@ -5,14 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSightApplication {
+public class SprintSightApplication {
 
 	public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 
-        SpringApplication.run(SpringSightApplication.class, args);
+        SpringApplication.run(SprintSightApplication.class, args);
 	}
 
 }
