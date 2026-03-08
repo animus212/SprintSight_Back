30d2dea4500126f4c834cd6988a55ec82951ab91
animus <mohanadhussein536@gmail.com>
2026-03-08 23:41:06 +0200

    added jwt tokens

--- src/main/java/com/example/SprintSight/Configurations/SecurityConfiguration.java
+++ src/main/java/com/example/SprintSight/Configurations/SecurityConfiguration.java
@@ -1,17 +1,25 @@
 package com.example.SprintSight.Configurations;
 
+import com.example.SprintSight.Filters.JwtFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -20,6 +28,8 @@ public class SecurityConfiguration {
                         .requestMatchers("/register", "/login").permitAll()
                         .anyRequest().authenticated()
                 )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
--- src/main/java/com/example/SprintSight/Controllers/UserController.java
+++ src/main/java/com/example/SprintSight/Controllers/UserController.java
@@ -2,16 +2,15 @@ package com.example.SprintSight.Controllers;
 
 import com.example.SprintSight.DTOs.LoginDTO;
 import com.example.SprintSight.Entities.User;
-import com.example.SprintSight.Security.UserPrincipal;
+import com.example.SprintSight.Services.JWTService;
 import com.example.SprintSight.Services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,38 +20,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
+    private final JWTService jwtService;
 
     @PostMapping("/register")
-    public User register(@Valid @RequestBody User user, HttpServletRequest httpServletRequest) {
+    public String register(@Valid @RequestBody User user, HttpServletRequest httpServletRequest) {
         user = userService.AddUser(user);
 
-        UserPrincipal principal = new UserPrincipal(user);
-
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
-
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-        httpServletRequest.getSession(true)
-                .setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
-
-        return user;
+        return jwtService.generateToken(user.getUsername());
     }
 
     @PostMapping("/login")
-    public ResponseEntity<User> login(@Valid @RequestBody LoginDTO loginDTO, HttpServletRequest httpServletRequest) {
+    public String login(@Valid @RequestBody LoginDTO loginDTO, HttpServletRequest httpServletRequest) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword())
         );
 
-        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+        if (authentication.isAuthenticated()){
+            return jwtService.generateToken(loginDTO.getUsername());
+        }
+        else{
+            return "failed";
+        }
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        httpServletRequest.getSession(true)
-                .setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
 
-        assert principal != null;
-        return ResponseEntity.ok(principal.getUser());
+//        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+//
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+//
+//        httpServletRequest.getSession(true)
+//                .setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+//
+//        assert principal != null;
+//        return ResponseEntity.ok(principal.getUser());
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "halloo";
     }
 }
