48abb6a1f2803afb97541577588a1206f6fcf3b9
Abd El-Rahman Ali <abdel292004@gmail.com>
2026-03-18 22:59:13 +0200

    Add RefreshToken logic to JwtService

---
+++ src/main/java/com/example/SprintSight/Services/JwtService.java
@@ -0,0 +1,109 @@
+package com.example.SprintSight.Services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.WebUtils;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${SprintSight.app.jwtSecret}")
+    private String jwtSecret;
+
+    private final String jwtCookie = "SprintSightJwtCookie";
+    private final String jwtRefreshCookie = "SprintSightJwtRefreshCookie";
+
+    public ResponseCookie generateJwtCookie(String username) {
+        String jwt = generateJwtToken(username);
+
+        return generateCookie(jwtCookie, jwt, "/api");
+    }
+
+    public ResponseCookie generateRefreshJwtCookie(String refreshToken) {
+        return generateCookie(jwtRefreshCookie, refreshToken, "/api/auth/refreshtoken");
+    }
+
+    public String getJwtFromCookies(HttpServletRequest request) {
+        return getCookieValueByName(request, jwtCookie);
+    }
+
+    public String getJwtRefreshFromCookies(HttpServletRequest request) {
+        return getCookieValueByName(request, jwtRefreshCookie);
+    }
+
+    public ResponseCookie getCleanJwtCookie() {
+        return ResponseCookie.from(jwtCookie, null).path("/api").build();
+    }
+
+    public ResponseCookie getCleanJwtRefreshCookie() {
+        return ResponseCookie.from(jwtRefreshCookie, null).path("/api/auth/refreshtoken").build();
+    }
+
+    public  String generateJwtToken(String username) {
+        int jwtExpiration = 15 * 60 * 1000;
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getKey())
+                .compact();
+    }
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean validateJwtToken(String token, UserDetails userDetails) {
+        final String userName = extractUserName(token);
+
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private SecretKey getKey() {
+        return Keys.hmacShaKeyFor(Base64.getUrlDecoder().decode(jwtSecret));
+    }
+
+    private ResponseCookie generateCookie(String name, String value, String path) {
+        return ResponseCookie.from(name, value).path(path).maxAge(24 * 60 * 60).httpOnly(true).build();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private String getCookieValueByName(HttpServletRequest request, String name) {
+        Cookie cookie = WebUtils.getCookie(request, name);
+
+        if (cookie != null) {
+            return cookie.getValue();
+        }
+
+        return null;
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+}
