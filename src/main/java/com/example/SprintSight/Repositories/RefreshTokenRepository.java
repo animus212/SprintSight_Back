68f3106ff662c72cdf203eb821490207ddc8d8bc
Abd El-Rahman Ali <abdel292004@gmail.com>
2026-03-22 18:21:10 +0200

    Edit Repositories

--- src/main/java/com/example/SprintSight/Repositories/RefreshTokenRepository.java
+++ src/main/java/com/example/SprintSight/Repositories/RefreshTokenRepository.java
@@ -1,16 +1,19 @@
 package com.example.SprintSight.Repositories;
 
 import com.example.SprintSight.Entities.RefreshToken;
-import com.example.SprintSight.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
-    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByTokenAndRevokedFalse(String token);
+    List<RefreshToken> findByUserIdAndRevokedFalse(UUID userId);
 
     @Modifying
-    int deleteByUser(User user);
+    @Transactional
+    int deleteByUserId(UUID userId);
 }
